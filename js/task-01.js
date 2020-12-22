0efc88c3859dc373cf81eb8278c85e4be1a1cb7d
Serhii Demchenko <dem.s.vl@gmail.com>
2020-12-22 11:21:47 +0200

    add

---
+++ js/task-01.js
@@ -0,0 +1,23 @@
+class Account {
+  constructor(obj) {
+    this.login = obj.login;
+    this.email = obj.email;
+  }
+  getInfo() {
+    console.log(`Login: ${this.login}, Email: ${this.email}`);
+  }
+}
+console.log(Account.prototype.getInfo); // function
+
+const mango = new Account({
+  login: 'Mangozedog',
+  email: 'mango@dog.woof',
+});
+mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+const poly = new Account({
+  login: 'Poly',
+  email: 'poly@mail.com',
+});
+
+poly.getInfo(); // Login: Poly, Email: poly@mail.com
